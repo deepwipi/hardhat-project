1437005b4d2b44b2e215592c7a4fcb32b09f4a3a
deepwipi <102171434+deepwipi@users.noreply.github.com>
2022-12-25 11:27:04 +0530

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: UNLICENSED
 
 // Solidity files have to start with this pragma.
-// It will be used by the Solidity compiler to validate its version.
+// It will be used by the Solidity compiler to validate its version .
 pragma solidity ^0.8.9;
 
 // We import this library to be able to use console.log
